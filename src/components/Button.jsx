5618b504f63c9e47e06a26a1820157c0c26c8279
Gboyega Daniel <119269023+GboyegaAkere@users.noreply.github.com>
2024-04-08 15:24:54 +0100

    worked and customize button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import { DatePicker, Button } from 'antd';
+import { StarOutlined, StarFilled, StarTwoTone } from '@ant-design/icons';
 
 const ButtonComponent = () => {
+    const [loading, setLoading] =React.useState(false)
+
+    const handleClicked = (e) => {
+        console.log("button clicked")
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 2000);
+    }
+
   return (
     <div className='flex flex-1 justify-center gap-6 mt-4'>
-     <Button type='primary'>Submit</Button>
+        <Button 
+            icon={<StarFilled/>} 
+            type='primary' 
+            loading={loading}  
+            style={{backgroundColor:"orange", color:"purple"}}
+            onClick={handleClicked}>Submit
+        </Button>
     </div>
   )
 }
