916251100d2b1a2dbc10fdbfbe20af4ad08e5c33
Gboyega Daniel <119269023+GboyegaAkere@users.noreply.github.com>
2024-04-08 14:03:00 +0100

    button component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { DatePicker, Button } from 'antd';
+
+const App = () => {
+ return (
+  <div className='flex flex-1 justify-center gap-6 mt-4'>
+    <Button type='primary'>Submit</Button>
+  </div>
+ )
+};
+
+export default App;
