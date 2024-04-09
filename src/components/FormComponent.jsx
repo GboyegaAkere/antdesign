da9647e788149b82a1b01218b5d23ac99280af7f
Gboyega Daniel <119269023+GboyegaAkere@users.noreply.github.com>
2024-04-09 02:39:31 +0100

    worked on the SUCCESS MESSGAE popup

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import {Form, Input,Button} from "antd"
+import {Form, Input,Button, message} from "antd"
 
 const FormComponent = () => {
     const onFinish = (e) =>{
         console.log(e)
+        setTimeout(() => {
+            message.success("Login successful")
+        }, 1000);
     }
+
   return (
     <div className='flex justify-center mt-10'>
       <Form className='w-[400px]' onFinish={onFinish}>
